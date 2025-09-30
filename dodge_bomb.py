41b767287c8a6ac6ab8da15c2604c7d9fc557e0e
c0a24035e3 <c0a24035e3@edu.teu.ac.jp>
2025-09-30 14:57:04 +0900

    画面内外判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,13 @@ DELTA = {
 
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def check_bound(rct: pg.Rect)->tuple[bool,bool]:
+    yoko,tate = True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
 
 
 def main():
@@ -64,8 +71,15 @@ def main():
         #   sum_mv[0] += 5
         
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        kk_rct.move_ip(vx,vy)
+        bb_rct.move_ip(vx,vy)
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
